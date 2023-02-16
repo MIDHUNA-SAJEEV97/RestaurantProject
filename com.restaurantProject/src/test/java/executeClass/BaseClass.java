8293a4c02288325b464133f9d739bbc6e94b0446
user <user@MIDHUNA.bbrouter>
2023-02-16 23:45:52 +0530

    second commit

--- com.restaurantProject/src/test/java/executeClass/BaseClass.java
+++ com.restaurantProject/src/test/java/executeClass/BaseClass.java
@@ -2,10 +2,11 @@ package executeClass;
 
 import org.testng.annotations.Test;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import utilities.ScreenShotClass;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,10 +16,17 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 	WebDriver driver;
+	ScreenShotClass sh;
+	
 	public static Properties property;          //to read data
 	
 	public static void readProperty() throws IOException {
@@ -28,19 +36,47 @@ public class BaseClass {
 	property.load(file);
 	
 	}
+	
+	
+	
+	@BeforeSuite(alwaysRun=true)
+	public void createReport(final ITestContext testContext) {
+		  extendReport.ExtentManager.createInstance().createTest(testContext.getName(), "message");
+	  }
+	
+	
  
 	@BeforeMethod
-	public void beforeMethod() throws IOException {
-		readProperty();
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+	@Parameters({"browser"})
+	public void beforeMethod(String browserValue) throws IOException {
+		
+		readProperty();    //to load url
+		
+		if(browserValue.equalsIgnoreCase("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else if(browserValue.equalsIgnoreCase("edge")) 
+		{
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		
 		driver.get(property.getProperty("url"));
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 
 	@AfterMethod
-	public void afterMethod() {
+	public void afterMethod(ITestResult itr) throws IOException {
+		
+		if(itr.getStatus()==ITestResult.FAILURE) 
+		{
+			sh=new ScreenShotClass();
+			sh.takeScreenShot(driver, itr.getName());
+		}
+		driver.close();
 		driver.quit();
 	}
 
